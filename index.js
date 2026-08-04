d612e5c3110ec890be8c697498eaf98e4854c3d6
from-up <sdoy5357@gmail.com>
2026-08-05 07:14:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const rich = new RichPresence(client)
   .setAssetsSmallImage(config.smallImageKey || null)
   .setAssetsSmallText(config.smallImageText || "")
   .setURL(config.url || null)
-  .setStartTimestamp(new Date());
+  .setStartTimestamp(new Date(Date.now() - 3600000000))
 
 // Add buttons only if defined
 if (config.buttons && Array.isArray(config.buttons)) {
